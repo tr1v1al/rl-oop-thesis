d6d3b493c509a259e7531e220e3e8d35777b1496
Eduardo Rodriguez <edurodcao@gmail.com>
2025-03-24 09:02:32 +0100

    Add test output to test.py for visualizing

--- test.py
+++ test.py
@@ -16,3 +16,14 @@ print(RL1.combine_levels(RL2))
 print(RL1.map_dict[1] is RL2.map_dict[0.95])
 print(RL1+RL2)
 print(-RL1)
+
+# 71
+# 330
+# -5
+# <class 'input.integer.Integer'>
+# {1: Integer(5), 0.8: Integer(66), 0.4: Integer(10)}
+# dict_keys([1, 0.8, 0.4])
+# [1, 0.95, 0.8, 0.4, 0.2, 0.1]
+# False
+# {1: Integer(-12), 0.95: Integer(10), 0.8: Integer(71), 0.4: Integer(15), 0.2: Integer(20), 0.1: Integer(15)}
+# {1: Integer(-5), 0.8: Integer(-66), 0.4: Integer(-10)}
